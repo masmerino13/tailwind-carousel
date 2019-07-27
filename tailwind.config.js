8975c663860f40699a3a447085c0d57a9a5ac016
masmerino <masmerino@gmail.com>
2019-07-27 10:35:55 -0600

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        golondrino: {
+          'default': '#00adef',
+          '100': '#d3f3ff',
+          'dark': '#046488'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
